15f66774f9cb65bbe94c029d3580a0c74be97f58
Davit-Glunchadze <davitiglunchadze@gmail.com>
2025-09-02 01:12:35 +0400

    Add name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiz/',  // <---- რეპოზიტორიის სახელი
+  base: '/quiz-app/',  // <---- რეპოზიტორიის სახელი
 })
 
